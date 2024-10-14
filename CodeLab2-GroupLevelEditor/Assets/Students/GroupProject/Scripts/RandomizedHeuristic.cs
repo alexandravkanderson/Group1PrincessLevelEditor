ab220cc6d8453fb5d5437a03c58291355ca8bc56
ESP-YRI <sap637@nyu.edu>
2024-10-14 19:38:58 -0400

    Updated Princess Test Scene
    Made the randomized heuristic script more variable so one princess leans towards the manhattan script while another leans towards a randomized script, making each princess more or less good at pathfinding.

--- CodeLab2-GroupLevelEditor/Assets/Students/GroupProject/Scripts/RandomizedHeuristic.cs
+++ CodeLab2-GroupLevelEditor/Assets/Students/GroupProject/Scripts/RandomizedHeuristic.cs
@@ -1,19 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RandomizedHeuristic : HeuristicScript
 {
-    public override float Heuristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript){
-        if (Random.Range(0f, 1f) > .5f)
+    public float upperBound = .6f;
+    public float lowerBound = .2f;
+    public override float Heuristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript)
+    {
+        float randomNumber = Random.Range(0f, 1f);
+        if (randomNumber > upperBound)
         {
             x = Mathf.RoundToInt(transform.position.x - goal.x);
             y = Mathf.RoundToInt(transform.position.y - goal.y);
             return 1 * (x + y);
         }
-        else 
+        else if (randomNumber <= upperBound && randomNumber > lowerBound) 
         {
             return Random.Range(0, 1000);
+        }
+
+        else
+        {
+            x = Mathf.RoundToInt(transform.position.x - goal.x);
+            y = Mathf.RoundToInt(transform.position.y - goal.y);
+            return 1 * Mathf.Sqrt(x * x + y * y);
         }
     }
 }
